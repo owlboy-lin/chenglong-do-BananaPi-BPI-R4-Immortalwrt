bd5e9a32ecd86e75c5e44b5c24cf9c3b81e6d061
owlboy-lin <298076473@qq.com>
2025-05-08 23:03:56 +0800

    Update diy-part1-test.sh

--- immortalwrt/diy-part1-test.sh
+++ immortalwrt/diy-part1-test.sh
@@ -33,9 +33,10 @@ merge_package https://github.com/DHDAXCW/lede-rockchip lede-rockchip/package/wwa
 merge_package https://github.com/kenzok8/jell jell/luci-app-serverchan
 merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/relevance/ddnsto
 merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-ddnsto
-merge_package https://github.com/kenzok8/jell jell/quickstart
-merge_package https://github.com/kenzok8/jell jell/luci-app-quickstart
-merge_package https://github.com/kenzok8/jell jell/luci-lib-js
+merge_package "-b Immortalwrt https://github.com/shidahuilang/openwrt-package" openwrt-package/luci-app-quickstart
+# merge_package https://github.com/kenzok8/jell jell/quickstart
+# merge_package https://github.com/kenzok8/jell jell/luci-app-quickstart
+# merge_package https://github.com/kenzok8/jell jell/luci-lib-js
 merge_package https://github.com/kenzok8/jell jell/luci-app-webadmin
 popd
 
