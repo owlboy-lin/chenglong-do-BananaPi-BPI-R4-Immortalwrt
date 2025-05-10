ff314fa707a99d903244a8e1f71cf4e5fc885c78
owlboy-lin <298076473@qq.com>
2025-05-10 12:57:27 +0800

    Update diy-part2-1.sh

--- immortalwrt/diy-part2-1.sh
+++ immortalwrt/diy-part2-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.89.249/g' package/base-files/files/bin/config_generate
 
 
 echo "
